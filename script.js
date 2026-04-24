4bd3aed694e21cf72dd0cd5e65eada428cd15e0e
Kingarthur404 <operations@breakfrontservices.com>
2026-04-24 15:40:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,49 @@
-body {
-  background: #f9f7fe;
-  font-family: "Roboto", sans-serif;
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
-.weather-app {
-  background: white;
-  max-width: 600px;
-  margin: 60px auto;
-  padding: 30px;
-  border-radius: 10px;
-  box-shadow: 0 30px 50px rgba(65, 50, 100, 0.08);
+function displayWeather(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let city = response.data.city;
+
+  document.querySelector("#current-city").innerHTML = city;
+  document.querySelector("#temperature").innerHTML = temperature;
 }
 
-header {
-  border-bottom: 1px solid #f9f7fe;
-  padding-bottom: 30px;
+function search(event) {
+  event.preventDefault();
+
+  let apiKey = "0d3379b46oc5bbf60f2b70t899b1a212";
+  let city = document.querySelector("#search-input").value;
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
 }
 
-form {
-  display: flex;
-  gap: 10px;
-}
+document.querySelector("#current-date").innerHTML = formatDate(new Date());
 
-input[type="search"] {
-  background-color: #f9f7fe;
-  border: none;
-  padding: 20px;
-  width: 75%;
-  border-radius: 6px;
-  font-size: 16px;
-}
-
-input[type="submit"] {
-  background-color: #885df1;
-  color: white;
-  border: none;
-  padding: 20px;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-}
-
-main {
-  padding: 30px 0;
-}
-
-#current-city {
-  font-size: 38px;
-  font-weight: 900;
-  margin: 0;
-}
-
-#current-date {
-  color: rgba(39, 33, 66, 0.4);
-  display: block;
-  margin-bottom: 10px;
-}
-
-#temperature {
-  font-size: 60px;
-  font-weight: bold;
-}
-
-footer {
-  border-top: 1px solid #f9f7fe;
-  text-align: center;
-  padding-top: 15px;
-  color: rgba(39, 33, 66, 0.4);
-}
-
-a {
-  color: #885df1;
-}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
