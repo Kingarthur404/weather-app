6c43270858ec8a5eae4f11df105ca82c28b7c2e0
Kingarthur404 <operations@breakfrontservices.com>
2026-04-24 15:51:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,25 +25,40 @@ function formatDate(date) {
 }
 
 function displayWeather(response) {
-  let temperature = Math.round(response.data.temperature.current);
   let city = response.data.city;
+  let temperature = Math.round(response.data.temperature.current);
+  let humidity = Math.round(response.data.temperature.humidity);
+  let windSpeed = Math.round(response.data.wind.speed);
+  let description = response.data.condition.description;
+  let iconUrl = response.data.condition.icon_url;
 
   document.querySelector("#current-city").innerHTML = city;
   document.querySelector("#temperature").innerHTML = temperature;
+  document.querySelector("#humidity").innerHTML = `${humidity}%`;
+  document.querySelector("#wind-speed").innerHTML = `${windSpeed}km/h`;
+  document.querySelector("#weather-description").innerHTML = description;
+
+  let iconElement = document.querySelector("#weather-icon");
+  iconElement.setAttribute("src", iconUrl);
+  iconElement.setAttribute("alt", description);
 }
 
-function search(event) {
-  event.preventDefault();
-
+function searchCity(city) {
   let apiKey = "0d3379b46oc5bbf60f2b70t899b1a212";
-  let city = document.querySelector("#search-input").value;
-
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeather);
 }
 
+function handleSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
+
 document.querySelector("#current-date").innerHTML = formatDate(new Date());
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSearch);
+
+searchCity("Paris");
